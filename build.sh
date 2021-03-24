fbd7d486c9c2a835b20893c087338622a67480d1
jyotir <jadityav@gmail.com>
2021-03-24 21:45:25 +0530

    new scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat ./templates/top.html ./content/index.html ./templates/bottom.html > ./docs/index.html
+cat ./templates/top.html ./content/order.html ./templates/bottom.html > ./docs/order.html
+cat ./templates/top.html ./content/prices.html ./templates/bottom.html > ./docs/prices.html
